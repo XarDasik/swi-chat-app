c6bacb45c8e7fa8a4fd1edfaa9e0dac1da3032b3
Z4jo <ondrataus@seznam.cz>
2022-10-21 13:38:22 +0200

    test bro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import LoginPage from "./loginPage";
-
+import LoginPage from "./LoginPage";
+//TEST :BRO
 const Home: NextPage = () => {
   return (
     <>
